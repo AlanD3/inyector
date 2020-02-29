6bbe08df4bff7141015f4133131a7ac725478db2
Alan Delgado <alan.delgado.duran@gmail.com>
2020-02-29 13:36:05 -0700

    feat: adds classes to public api

--- projects/inyector/src/public-api.ts
+++ projects/inyector/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of inyector
  */
 
+export * from './lib/services/component-controller/component-controller.service';
+export * from './lib/classes/component-event/component-event';
+export * from './lib/classes/inyector-params/inyector-params';
 export * from './lib/inyector.service';
 export * from './lib/inyector.module';
-export * from './lib/services/component-controller/component-controller.service';
